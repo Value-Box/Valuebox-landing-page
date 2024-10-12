d4a26567312d8725a43b30a7a0a676dae7e7fc58
Muhammad Ramzan <mr4323992@gmail.com>
2024-10-12 12:41:22 +0500

    minor changes  and add js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginBtn = document.querySelector('.loginBtn');
+console.log('hello')
+
+console.log(loginBtn);
+
+// loginBtn.addEventListener('click', () => {
+//     console.log('Button clicked');  // Check if this logs on click
+//     window.location.href = 'dashboardLogin.html';
+// });
+
+function loginPage(){
+    window.location.href = 'dashboardLogin.html';
+}
