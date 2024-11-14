6edff93b2b2d65373e9d20d4bb69589b0541c6ac
Sajeela-Waseem <waseemsajeela123@gmail.com>
2024-11-14 16:53:51 +0500

    add arrow button

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ const loginBtn = document.querySelector(".loginBtn");
 //       logInForm.style.display = 'none';
 //     }
 //   }
+
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 function loginPage() {
   window.location.href = `dashboardLogin.html`;
 }
